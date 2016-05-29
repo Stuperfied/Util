56fee9b6a001da585f4e7fe9d7bc7135156f7918
Stuperfied <hurt_domain@hotmail.com>
2016-05-29 21:46:30 +1000

    Update RegionPaster.java

--- Drop/RegionPaster.java
+++ Drop/RegionPaster.java
@@ -5,25 +5,50 @@ import org.bukkit.plugin.Plugin;
 
 import com.sk89q.worldedit.EditSession;
 import com.sk89q.worldedit.LocalSession;
+import com.sk89q.worldedit.Vector;
 import com.sk89q.worldedit.WorldEdit;
 import com.sk89q.worldedit.WorldEditException;
 import com.sk89q.worldedit.bukkit.WorldEditPlugin;
 import com.sk89q.worldedit.command.ClipboardCommands;
 import com.sk89q.worldedit.entity.Player;
 import com.sk89q.worldedit.extent.clipboard.Clipboard;
+import com.sk89q.worldedit.function.operation.ForwardExtentCopy;
+import com.sk89q.worldedit.function.operation.Operation;
+import com.sk89q.worldedit.function.operation.Operations;
+import com.sk89q.worldedit.regions.Region;
 import com.sk89q.worldedit.session.ClipboardHolder;
 
 
 public class RegionPaster {
-	public void paste(ClipboardHolder clipboard, Player player) throws WorldEditException {
+	public void paste(ClipboardHolder holder, EditSession editSession, Vector origin) throws WorldEditException {
 
 		WorldEditPlugin worldEdit = (WorldEditPlugin) Bukkit.getServer().getPluginManager().getPlugin("WorldEdit");
 		WorldEdit w = worldEdit.getWorldEdit();
         
-	    EditSession es = new LocalSession().createEditSession(player);
-	    LocalSession session = WorldEdit.getInstance().getSessionManager().get(player);
+        Clipboard clipboard = holder.getClipboard();
+        Region region = clipboard.getRegion();
+
+        
+        ForwardExtentCopy copy = new ForwardExtentCopy(clipboard, region, clipboard.getOrigin(), editSession.getWorld(), origin);
+
+        Operations.completeLegacy(copy);
+        
+        // atOrigin ? clipboard.getOrigin() : 
+        //Vector to = origin;
+        //Operation operation = holder
+                //.createPaste(editSession, editSession.getWorld().getWorldData())
+                //.to(to)
+                //.ignoreAirBlocks(false)
+                //.build();
+        //Operations.completeLegacy(operation);
+		
+		
+		
+		
+	    // EditSession es = new LocalSession().createEditSession(player);
+	    // LocalSession session = WorldEdit.getInstance().getSessionManager().get(player);
 			
-	    ClipboardCommands clipboardCommands = new ClipboardCommands(w);
-	    clipboardCommands.paste(player, session, es, false, false, false);		
+	    // ClipboardCommands clipboardCommands = new ClipboardCommands(w);
+	    // clipboardCommands.paste(player, session, es, false, false, false);		
 	}
 }
