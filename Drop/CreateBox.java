9a1cf0ee346e7af546403924ccc27817bf7eda22
Stuperfied <hurt_domain@hotmail.com>
2016-05-29 14:48:27 +1000

    Update CreateBox.java
    Bug Fixes...
    Still not working...

--- Drop/CreateBox.java
+++ Drop/CreateBox.java
@@ -14,18 +14,24 @@
 			pos2 = BukkitUtil.toVector(vec2);
 			
 			CuboidRegion region = new CuboidRegion(world, pos2, pos2);
+						
 			RegionCopier copier = new RegionCopier();
 			ClipboardHolder clipboard = null;
+			LocalPlayer localPlayer = worldEdit.wrapPlayer((org.bukkit.entity.Player) player);
+			LocalSession session = WorldEdit.getInstance().getSessionManager().get(localPlayer);
+			com.sk89q.worldedit.Vector origin = null;
 			try {
-				clipboard = copier.copyRegion(region);
+				origin = session.getPlacementPosition(localPlayer);
+				clipboard = copier.copyRegion(region, origin);
+				RegionPaster paster = new RegionPaster();
+				paster.paste(clipboard, localPlayer);
+			} catch (IncompleteRegionException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			} catch (WorldEditException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			RegionPaster paster = new RegionPaster();
-			try {
-				paster.paste(clipboard, (com.sk89q.worldedit.entity.Player) player);
-			} catch (WorldEditException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			
+		}
+	}
