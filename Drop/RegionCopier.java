51892c145fbecafa36a3ee4b2b8a7156adefaa7d
Stuperfied <hurt_domain@hotmail.com>
2016-05-29 12:04:05 +1000

    Update RegionCopier.java
    Bug fixes.
    Still not working..

--- Drop/RegionCopier.java
+++ Drop/RegionCopier.java
@@ -5,29 +5,38 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.plugin.Plugin;
 
 import com.sk89q.worldedit.EditSession;
+import com.sk89q.worldedit.LocalPlayer;
 import com.sk89q.worldedit.LocalSession;
 import com.sk89q.worldedit.LocalWorld;
 import com.sk89q.worldedit.WorldEdit;
 import com.sk89q.worldedit.WorldEditException;
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
 import com.sk89q.worldedit.command.ClipboardCommands;
 import com.sk89q.worldedit.entity.Player;
-import com.sk89q.worldedit.extent.clipboard.Clipboard;
-import com.sk89q.worldedit.regions.Region;
+import com.sk89q.worldedit.regions.CuboidRegion;
 import com.sk89q.worldedit.session.ClipboardHolder;
+import com.sk89q.worldedit.session.SessionManager;
 
 public class RegionCopier {
 
-	public ClipboardHolder copyRegion(Region region) throws WorldEditException {
+	public ClipboardHolder copyRegion(CuboidRegion region) throws WorldEditException {
 
-		Plugin worldEditPlugin = Bukkit.getServer().getPluginManager().getPlugin("WorldEdit");
-        WorldEdit worldEdit = (WorldEdit) worldEditPlugin;
+		WorldEditPlugin worldEdit = (WorldEditPlugin) Bukkit.getServer().getPluginManager().getPlugin("WorldEdit");
+		WorldEdit w = worldEdit.getWorldEdit();
         
 	    EditSession es = new EditSession((LocalWorld) region.getWorld(), Integer.MAX_VALUE);
 		
 		OfflinePlayer offlinePlayer = null;
 		Player player = (Player) offlinePlayer;
-		LocalSession session = new LocalSession();
-	    ClipboardCommands clipboard = new ClipboardCommands(worldEdit);
+		
+		// LocalSession session = new LocalSession();
+	    // LocalSession session = WorldEdit.getInstance().getSessionManager().get(player);
+		
+        LocalPlayer localPlayer = worldEdit.wrapPlayer((org.bukkit.entity.Player) player);
+        LocalSession session = w.getSession(localPlayer);
+		
+	    
+	    ClipboardCommands clipboard = new ClipboardCommands(w);
 	    clipboard.copy(player, session, es, region, false, null);
 	    ClipboardHolder holder = session.getClipboard();
 		return holder;
@@ -35,3 +44,4 @@ public class RegionCopier {
 	
 
 }
+
